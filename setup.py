b12f94abe7928e4ac38a7e971d2928d473ae7530
Shubham <shubhamgohel28@gmail.com>
2024-08-08 16:09:23 +0530

    Project structure,logger,exception

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ def get_requirements(file_path:str)->List[str]:
         requirements = [req.replace("\n","") for req in requirements]
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
+    return requirements
 
 setup(
     name='Ml_generic_project',
